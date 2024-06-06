4ddeae3e7ae0a8dd4a7a701b6ce43f7b81054032
Harry-B-Smith <harrybsmithiv@gmail.com>
2024-06-06 14:44:42 -0400

    initial commit with fully working chart and hardcoded data

--- health-history/src/App.js
+++ health-history/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import * as apiresult from './res.json'
+import Chart from './components/Chart'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      chartData: {}
+    };
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData() {
+
+    this.setState({
+      chartData: {
+        labels: ['2019', '2020', '2021', '2022', '2023', '2024'],
+        datasets:[
+          {
+            label: 'Triglycerides',
+            data:[
+                  150,
+                  200,
+                  125,
+                  100,
+                  90,
+                  75
+                ],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }       
+
+        ],
+        
+      }
+    })
+
+  }
+
+  render() {
+    let res = apiresult;
+    return (
+      <div>       
+       Hello {res.name}
+         <Chart chartData={this.state.chartData}/>
+      </div>
+    );
+  }
 }
 
 export default App;
